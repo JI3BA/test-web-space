21986429033ba83f4ed29e1a528adc949c98021c
JI3BA <lyovkin_artyom@mail.ru>
2023-11-16 01:36:30 +0300

    Slides type was added

--- src/app/components/Slider/Slider.tsx
+++ src/app/components/Slider/Slider.tsx
@@ -4,10 +4,11 @@ import m from '../../page.module.css'
 import s from '../../styles/Slider.module.css'
 import {useState} from "react";
 import {nunito} from "@/app/fonts/fonts";
+import {SlideType} from "@/app/types/SlideType";
 
 const Slider = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const slides = [
+    const [currentSlide, setCurrentSlide] = useState<number>(0);
+    const slides: SlideType[] = [
         { id: 1, title: "Оценка дизайнеру:", description: "Не заслужил, там криво, там непонятно", unit: '3', unitDesc:'и не более'},
         { id: 2, title: "Сложность задания", description: "Миссия с вертолетиком была легче..", unit: '5', unitDesc: 'звезд' },
         { id: 3, title: "Потребовалось на тестовое задание:", description: "Справился быстро, забыл пообедать", unit: '9', unitDesc: 'часов' },
