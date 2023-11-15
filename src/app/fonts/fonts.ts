77892f2c4982585713a1be817dd01b1074603ac7
JI3BA <lyovkin_artyom@mail.ru>
2023-11-15 20:24:19 +0300

    all fonts were added to project

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Nunito, Quicksand } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' })
+export const nunito = Nunito({ subsets: ['latin'], display: 'swap' })
+export const quicksand = Quicksand({ subsets: ['latin'], display: 'swap' })
