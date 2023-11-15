5802c788438a1e43862aceefc12ddb83075a54c6
JI3BA <lyovkin_artyom@mail.ru>
2023-11-16 02:55:30 +0300

    next js config was updated (basePath was removed and typescript was added)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/github-pages',
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
