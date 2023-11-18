7c3293251daaf1a1906b88bcf5d23cad32bfa89d
JI3BA <lyovkin_artyom@mail.ru>
2023-11-18 12:58:55 +0300

    Map component was added to Main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import styles from './page.module.css'
 import Slider from "@/app/components/Slider/Slider";
+import GMap from "@/app/components/Map/GMap";
+
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
-      <Slider />
+        <Slider />
+        <GMap />
     </main>
   )
 }
