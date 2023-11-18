73c47cad0ddbb9d2c9c8332add0e5f60290845f8
JI3BA <lyovkin_artyom@mail.ru>
2023-11-18 12:58:24 +0300

    Map component was added

---
+++ src/app/components/Map/GMap.tsx
@@ -0,0 +1,15 @@
+
+
+const GMap = () => {
+    return(
+        <>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16677.13317879691!2d27.557545080012996!3d53.89344125563347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbcfd35b1e6ad3%3A0xb61b853ddb570d9!2z0JzQuNC90YHQug!5e1!3m2!1sru!2sby!4v1700301015617!5m2!1sru!2sby"
+                style={{width:"600", height:"450", style:"border:0", allowFullScreen:""}}
+                referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
+        </>
+    )
+}
+
+export default GMap
